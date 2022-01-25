137322d57988f52338d4eb160d9d16ff4098192c
Matthieu THIBAUT <matthieu.thibaut@edu.devinci.fr>
2022-01-25 20:39:07 +0100

    featured 3 and 4

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -9,6 +9,11 @@ let currentPagination = {};
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
 const selectBrand = document.querySelector('#brand-select');
+const selectRecentProducts = document.querySelector('#recentProducts-select');
+const selectReasonablePrice = document.querySelector('#reasonablePrice-select');
+
+const selectSort = document.querySelector('#sort-select');
+
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
 
@@ -141,7 +146,45 @@ selectBrand.addEventListener('change', event => {
   var brand = event.target.value;
   fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
     .then(setCurrentProducts)
-    .then(() => currentProducts = currentProducts.filter(element => element.brand === brand))
+    .then(() => { if (brand !== 'All') {currentProducts = currentProducts.filter(element => element.brand === brand)}
+    })
+    .then(() => render(currentProducts, currentPagination));
+});
+
+// SELECT RECENTLY RELEASED PRODUCTS 
+selectRecentProducts.addEventListener('change', event => {
+  var filterRecentProducts = event.target.value;
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+    .then(setCurrentProducts)
+    .then(() => { if(filterRecentProducts === 'On') {
+      currentProducts = currentProducts.filter(element => (new Date().getTime() - new Date(element.released).getTime())/(24*60*60*1000) < 14)} 
+    })
+    .then(() => render(currentProducts, currentPagination));
+});
+
+// SELECT REASONABLE PRICE FILTER
+selectReasonablePrice.addEventListener('change', event => {
+  var filterReasonablePrice = event.target.value;
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+    .then(setCurrentProducts)
+    .then(() => { if(filterReasonablePrice === 'On') {
+      currentProducts = currentProducts.filter(element => element.price < 50)} 
+    })
+    .then(() => render(currentProducts, currentPagination));
+});
+
+// SELECT SORT  FILTER
+selectSort.addEventListener('change', event => {
+  var sort = event.target.value;
+  console.log(sort);
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+    .then(setCurrentProducts)
+    .then(() => { if (sort === 'price-desc') {currentProducts = currentProducts.sort( (e1, e2) => { return e1.price < e2.price}) }
+                  else if (sort === 'price-asc') {currentProducts = currentProducts.sort( (e1, e2) => { return e1.price > e2.price}) }
+                  else if (sort === 'date-desc') {currentProducts = currentProducts.sort( (e1, e2) => { return new Date(e1.released).getTime() < new Date(e2.released).getTime()}) }
+                  else if (sort === 'date-asc') {currentProducts = currentProducts.sort( (e1, e2) => { return new Date(e1.released).getTime() > new Date(e2.released).getTime()}) }
+
+    })
     .then(() => render(currentProducts, currentPagination));
 });
 
@@ -149,14 +192,13 @@ document.addEventListener('DOMContentLoaded', () =>
   fetchProducts()
     .then(setCurrentProducts)
     .then(() => render(currentProducts, currentPagination))
+    .then(() => addBrandsInSelectBox(getDisplayedBrands())) 
+    .then(() => addRecentlyReleasedFilterInSelectBox())
+    .then(() => addResonablePriceFilterInSelectBox())
 );
 
 
-
-
-
 // Feature 1 - Browse pages
-
 
 // Feature 2 - Filter by brands
 
@@ -180,6 +222,38 @@ const addBrandsInSelectBox = (brands) => {
 
 }
 
-
 //Feature 3 - Filter by recent products
+const addRecentlyReleasedFilterInSelectBox = () => {
+  var selectBox = document.getElementById('reasonablePrice-select');
+  selectBox.options.length = 0;
+  selectBox.options.add(new Option("Off", "Off", true));
+  selectBox.options.add(new Option("On", "On", false));
+}
+
+//Feature 4 - Filter by reasonable price
+const addResonablePriceFilterInSelectBox = () => {
+  var selectBox = document.getElementById('recentProducts-select');
+  selectBox.options.length = 0;
+  selectBox.options.add(new Option("Off", "Off", true));
+  selectBox.options.add(new Option("On", "On", false));
+}
+
+//Feature 5 - Sort by price
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
