cceab0ff6a745f63e44701dfd0f1f258e159b4c6
Matthieu THIBAUT <matthieu.thibaut@edu.devinci.fr>
2022-02-06 16:01:14 +0100

    if else depending on brand link

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,16 +1,32 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const adressbrand = require('./sources/adressbrand');
+const montlimartbrand = require('./sources/montlimartbrand');
+
+// https://adresse.paris/630-toute-la-collection
 
 async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    var products = []
+
+    if(eshop.includes('dedicated')){
+      products = await dedicatedbrand.scrape(eshop);
+    }
+    else if(eshop.includes('adress')){
+      products = await adressbrand.scrape(eshop);
+    }
+    else if(eshop.includes('adress')){
+      products = await montlimartbrand.scrape(eshop);
+    }
 
     console.log(products);
     console.log('done');
     process.exit(0);
-  } catch (e) {
+  } 
+  
+  catch (e) {
     console.error(e);
     process.exit(1);
   }
