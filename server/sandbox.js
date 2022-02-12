d58ebdb46513a1ea819d48509b18c9b195133f97
Matthieu THIBAUT <matthieu.thibaut@edu.devinci.fr>
2022-02-12 16:56:27 +0100

    use module from dedicated api

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,18 +1,23 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
+
+// const dedicatedbrand = require('./sources/dedicatedbrand');
+const test = require('./sources/test');
 const adressbrand = require('./sources/adressbrand');
 const montlimartbrand = require('./sources/montlimartbrand');
 
 // https://adresse.paris/630-toute-la-collection
+// https://www.dedicatedbrand.com/en/loadfilter?category=men%2Fnews
+// https://www.montlimart.com/toute-la-collection.html
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/loadfilter?category=men%2Fnews') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
     var products = []
 
     if(eshop.includes('dedicated')){
-      products = await dedicatedbrand.scrape(eshop);
+      products = await test.scrape(eshop);
+      //products = await dedicatedbrand.scrape(eshop);
     }
     else if(eshop.includes('adress')){
       products = await adressbrand.scrape(eshop);
@@ -20,6 +25,8 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
     else if(eshop.includes('montlimart')){
       products = await montlimartbrand.scrape(eshop);
     }
+
+    
 
     console.log(products);
     console.log('done');
