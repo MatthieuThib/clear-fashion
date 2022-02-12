153108abf14c418a3b01089186f77846b5081fc9
MatthieuThib <matthieu.thibaut@edu.devinci.fr>
2022-02-12 17:05:11 +0100

    dedicated scrapping with dedicated api + export module

---
+++ server/sources/dedicatedbrand.js
@@ -0,0 +1,43 @@
+const fetch = require("node-fetch");
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+ module.exports.scrape = async url => {
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+            var products = []
+            const body = await response.json();
+            
+            body.products.forEach(element => {
+                if(element.length === undefined){ 
+                // if the length of the element is not defined, then it means that 
+                // the element is a json object and NOT an empty array.
+                    products.push(
+                        {
+                            name : element.name, 
+                            price : element.price.priceAsNumber, 
+                            image : element.image[0], 
+                            link : `https://www.dedicatedbrand.com/en/` + element.canonicalUri
+                        })
+                }
+            });
+
+            return products;
+        }
+  
+        console.error(response); 
+        return null;
+    } 
+    catch (error) {
+        console.error(error);
+        return null;
+    }
+  };
+
+
+
