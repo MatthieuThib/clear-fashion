c55a26c8cb3bab490b27c593414ce2a256400e08
MatthieuThib <matthieu.thibaut@edu.devinci.fr>
2022-02-28 11:19:29 +0200

    scraping function for loom items

--- server/sandbox.js
+++ server/sandbox.js
@@ -5,11 +5,13 @@ const dedicatedbrand = require('./sources/dedicatedbrand');
 const adressbrand = require('./sources/adressbrand');
 const montlimartbrand = require('./sources/montlimartbrand');
 const _1083brand = require('./sources/1083brand');
+const loombrand = require('./sources/loombrand');
 
 const urlAdresse =  'https://adresse.paris/630-toute-la-collection'; // + "?p=2";
 const urlDedicated = 'https://www.dedicatedbrand.com/en/loadfilter?category=men%2Fnews';
 const urlMontlimart = 'https://www.montlimart.com/toute-la-collection.html'; // + "?p=2";
 const url1083 = 'https://www.1083.fr/'; //homme.html?limit=108'; //+ "&p=2";
+const urlLoom = 'https://www.loom.fr/collections/vestiaire-'; //+ "homme";
 
 function saveAsJson (products){
   var json = JSON.stringify(products, null, 2);
@@ -59,12 +61,26 @@ async function browse1083 (url1083, numberOfPages = 2){
   return products;
 }
 
+async function browseLoom (urlLoom){
+  var products = [];
+  const categories = ['homme', 'femme']
+
+  for(let c = 0; c < categories.length; c++){
+    console.log(urlLoom + `${categories[c]}`);
+    var p = await loombrand.scrape(urlLoom + `${categories[c]}`);
+    products = products.concat(p);
+  }
+
+  return products;
+}
+
 async function browseAll (){
   var products = [];
   products = products.concat(await browseDedicated(urlDedicated)); // 1572 products
   products = products.concat(await browseAdresse(urlAdresse)); // 90 products
   products = products.concat(await browseMontlimart(urlMontlimart)); // 112 products
   products = products.concat(await browse1083(url1083)); // 322 products
+  products = products.concat(await browseLoom(urlLoom)); // 322 products
 
   return products;
 }
@@ -95,6 +111,10 @@ async function sandbox (eshop = 'all'){
 
     else if(eshop.includes('1083')){
       products = await browse1083(url1083)
+    }
+
+    else if(eshop.includes('loom')){
+      products = await browseLoom(urlLoom)
     }
 
     saveAsJson(products);
