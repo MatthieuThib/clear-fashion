535df724bc5ba44e7e1af0c44fb1b3003f605af7
MatthieuThib <matthieu.thibaut@edu.devinci.fr>
2022-03-13 13:03:02 +0100

    all products and id endpoints

--- server/api.js
+++ server/api.js
@@ -1,23 +1,45 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const db = require ('./db')
+const ObjectId = require('mongodb').ObjectId;
 
 const PORT = 8092;
-
 const app = express();
-
 module.exports = app;
 
 app.use(require('body-parser').json());
 app.use(cors());
 app.use(helmet());
-
 app.options('*', cors());
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+var products = "";
+
+app.get('/products', async(request, response) => {
+  products = await db.FindProducts()
+
+  response.send({"count" : products.length, "products" : products});
 });
 
-app.listen(PORT);
+app.get('/products/:id', async(request, response) => {
+  products = await db.FindProducts(
+    {'_id': new ObjectId(request.params.id)},
+    false);
+  response.send({"count" : products.length, "products" : products});
+})
 
-console.log(`📡 Running on port ${PORT}`);
+
+
+
+async function main(){
+  await db.OpenConnection();
+
+  app.listen(PORT, (err) => {
+    if (err) console.error('❌ Unable to connect the server: ', err);
+    console.log(` 📡 Running on port ${PORT}`);
+  });
+  
+}
+
+main()
+
